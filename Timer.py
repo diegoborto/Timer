132155f0d67113f926a8ed29626337ab9021171d
Diego <bortosix@outlook.it>
2025-11-11 15:19:13 +0100

    Update Timer.py
    Added end time and 3 beeps

--- Timer.py
+++ Timer.py
@@ -8,16 +8,17 @@ import winsound
 
 def beep(freq=1000, dur=200):
     system = platform.system()
-    if system == "Windows":
-        winsound.Beep(freq, dur)
-    elif system == "Linux":
-        os.system(f"beep -f {freq} -l {dur}")
-    elif system == "Darwin":  # macOS
-        os.system(f"play -n synth {dur/1000} sine {freq}")
-    else:
-        print("\a", end="")  # fallback
+    for i in range(3):
+        if system == "Windows":
+            winsound.Beep(freq, dur)
+        elif system == "Linux":
+            os.system(f"beep -f {freq} -l {dur}")
+        elif system == "Darwin":  # macOS
+            os.system(f"play -n synth {dur/1000} sine {freq}")
+        else:
+            print("\a", end="")  # fallback
 
-print('Timer')
+print(colored('Timer','blue'))
 lenght = input('How many minutes? ')
 message = input('Type the message: ')
 print('')
@@ -29,7 +30,11 @@ print('Timer started at', formatted_time, 'for ', lenght, ' minutes.')
 
 time.sleep(int(lenght)*60)
 
-print(colored('Timer done!','red'))
+now = datetime.now()
+current_time = now.time()
+formatted_time = now.strftime("%H:%M:%S")
+print('Timer done at ', formatted_time)
+print(colored('Stopped','red'))
 beep()
 print(colored('-----------------------------------------------------------------','blue'))
 print(colored(message,'blue'))
